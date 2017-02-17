de96471ed1d1ee0fb14d6a341f9f3390dd1476cf
dodoma <bigml@qq.com>
2017-02-17 17:16:13 +0800

    trace - trace line shorter

--- include/mtrace_macro.h
+++ include/mtrace_macro.h
@@ -6,29 +6,33 @@
  */
 __BEGIN_DECLS
 
+/*
+ * not c++, we don't use __PRETTY_FUNCTION__
+ */
+
 #define mtc_die(f,...)                                                  \
     do {                                                                \
-        mtc_msg(__PRETTY_FUNCTION__,__FILE__,__LINE__,MTC_DIE,f,##__VA_ARGS__); \
+        mtc_msg(__func__,__FILE__,__LINE__,MTC_DIE,f,##__VA_ARGS__); \
         exit(-1);                                                       \
     } while(0)
 
 #define mtc_foo(f,...) \
-    mtc_msg(__PRETTY_FUNCTION__,__FILE__,__LINE__,MTC_FOO,f,##__VA_ARGS__)
+    mtc_msg(__func__,__FILE__,__LINE__,MTC_FOO,f,##__VA_ARGS__)
 
 #define mtc_err(f,...) \
-    mtc_msg(__PRETTY_FUNCTION__,__FILE__,__LINE__,MTC_ERROR,f,##__VA_ARGS__)
+    mtc_msg(__func__,__FILE__,__LINE__,MTC_ERROR,f,##__VA_ARGS__)
 
 #define mtc_warn(f,...) \
-    mtc_msg(__PRETTY_FUNCTION__,__FILE__,__LINE__,MTC_WARNING,f,##__VA_ARGS__)
+    mtc_msg(__func__,__FILE__,__LINE__,MTC_WARNING,f,##__VA_ARGS__)
 
 #define mtc_dbg(f,...)  \
-    mtc_msg(__PRETTY_FUNCTION__,__FILE__,__LINE__,MTC_DEBUG,f,##__VA_ARGS__)
+    mtc_msg(__func__,__FILE__,__LINE__,MTC_DEBUG,f,##__VA_ARGS__)
 
 #define mtc_info(f,...) \
-    mtc_msg(__PRETTY_FUNCTION__,__FILE__,__LINE__,MTC_INFO,f,##__VA_ARGS__)
+    mtc_msg(__func__,__FILE__,__LINE__,MTC_INFO,f,##__VA_ARGS__)
 
 #define mtc_noise(f,...) \
-    mtc_msg(__PRETTY_FUNCTION__,__FILE__,__LINE__,MTC_NOISE,f,##__VA_ARGS__)
+    mtc_msg(__func__,__FILE__,__LINE__,MTC_NOISE,f,##__VA_ARGS__)
 
 
 __END_DECLS
